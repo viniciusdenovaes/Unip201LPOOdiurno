aa3b825c13a29856fe65d34e8ea3204863327dd1
Vinicius de Novaes Guimaraes Pereira <viniciusdenovaes@gmail.com>
2020-02-04 10:52:58 -0300

    testando objectos

---
+++ Exemplo01Cachorro/src/Cachorro.java
@@ -0,0 +1,14 @@
+class Cachorro{
+    String nome;
+    int idade;
+    double peso;
+    
+    void latir(){
+        if(idade < 20){
+            System.out.println("au au meu nome eh " + nome);
+        }
+        else{
+            System.out.println("AU AU MEU NOME EH " + nome);
+        }
+    }
+}
--- Exemplo01Cachorro/src/Teste.java
+++ Exemplo01Cachorro/src/Teste.java
@@ -1,5 +1,32 @@
 class Teste{
     public static void main(String args[]){
         System.out.println("Ola Pessoal!!!");
+        System.out.println("Vamos fazer um cachorro");
+        
+        Cachorro caozinho;
+        caozinho = new Cachorro();
+        
+        caozinho.nome = "tobias";
+        caozinho.idade = 18;
+        caozinho.peso = 20.0;
+        
+        caozinho.latir();
+        
+        Cachorro cao = new Cachorro();
+        
+        cao.nome = "fada";
+        cao.idade = 25;
+        cao.peso = 40.0;
+        
+        cao.latir();
+        
+        caozinho = cao;
+        
+        caozinho.nome = "jorge";
+        
+        cao.latir();
+        
+        
+        
     }
 }
